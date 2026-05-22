6a0d6b5352418304723c4f06944581901c5d7bd8
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-05-22 22:11:45 +0200

    allow resourses to the user middleware

---
+++ backend/src/middlewares/role.middleware.ts
@@ -0,0 +1,24 @@
+import type { Response, NextFunction } from 'express';
+
+import type { AuthRequest } from './auth.middleware.js';
+
+export function authorizeRole(...roles: string[]) {
+
+    return (req: AuthRequest, res: Response, next: NextFunction) => {
+
+        if (!req.user) {
+            return res.status(401).json({
+                error: 'Unauthorized'
+            });
+        }
+
+        if (!roles.includes(req.user.role)) {
+
+            return res.status(403).json({
+                error: 'Forbidden'
+            });
+        }
+
+        next();
+    };
+}
