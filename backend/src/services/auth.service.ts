a59d9e6eac16315eb041954034876c7fb73caa0b
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-05-22 22:14:43 +0200

    auth services

---
+++ backend/src/services/auth.service.ts
@@ -0,0 +1,44 @@
+import { createUser, getUserByEmail } from "../database/repositories/user.repo.js";
+import jwt from 'jsonwebtoken';
+import { comparePassword } from "../utils/password.js";
+
+export async function registerUser(username: string, email: string, password: string) {
+
+    const existing = await getUserByEmail(email);
+
+    if (existing) {
+        throw new Error("User already exists");
+    }
+
+    const userId = await createUser(username, email, password);
+
+    const token = jwt.sign(
+        { id: userId, email: email, role: 'user' },
+        process.env.JWT_SECRET!,
+        { expiresIn: "1h" }
+    );
+
+    return { token, userId, username, email };
+}
+
+export async function loginUser(email: string, password: string) {
+    const user = await getUserByEmail(email);
+
+    if (!user) {
+        throw new Error("Invalid credentials");
+    }
+
+    const match = await comparePassword(password, user.password);
+
+    if (!match) {
+        throw new Error("Invalid credentials");
+    }
+
+    const token = jwt.sign(
+        { id: user.id, email: user.email, role: user.role },
+        process.env.JWT_SECRET!,
+        { expiresIn: "1h" }
+    );
+
+    return { token, userId: user.id, username: user.username, email: user.email };
+}
