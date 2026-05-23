8d9a161cc027556ae5d0d145e2edfe2cea72d026
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-05-23 22:08:49 +0200

    creating database

---
+++ backend/src/database/database.ts
@@ -0,0 +1,15 @@
+import sqlite3 from "sqlite3";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const dbPath = path.join(__dirname, '..', '..', 'db', 'database.db');
+
+const db = new sqlite3.Database(dbPath, (err) => {
+    if (err) console.error(err.message);
+    else console.log('SQLite connected');
+})
+
+export default db
