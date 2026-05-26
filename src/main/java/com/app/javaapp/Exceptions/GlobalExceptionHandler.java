2eaef1bc84e2824e0e518cc1248514ea6f625781
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-05-26 14:43:15 +0200

    added a global error handling heloer

---
+++ src/main/java/com/app/javaapp/Exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.app.javaapp.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, Object>> handleRuntimeException(RuntimeException ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("timestamp", LocalDateTime.now());
+        errorResponse.put("message", ex.getMessage());
+        errorResponse.put("status", HttpStatus.NOT_FOUND.value());
+
+        System.out.println("Got an error: " + errorResponse);
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+}
