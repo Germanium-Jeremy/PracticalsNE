d9f66cf690ed4fcced9abe1b4b6cc35589a6ff7d
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-05-27 19:42:30 +0200

    jwt DTO

--- src/main/java/com/app/javaapp/DTO/JwtResponse.java
+++ src/main/java/com/app/javaapp/DTO/JwtResponse.java
@@ -1,4 +1,46 @@
 package com.app.javaapp.DTO;
 
+
+import java.util.Set;
+
 public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String username;
+    private String email;
+    private Set<String> roles;
+
+    public JwtResponse(String token, Long id, String username, String email, Set<String> roles) {
+        this.token = token;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+    // getters
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
 }
