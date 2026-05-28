1a130af188628f714f929ae6b496feb7f381d935
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-05-28 21:54:03 +0200

    created Home page for redirection after authentication

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,32 @@
+import { useNavigate } from 'react-router-dom';
+
+import Button from '../components/ui/Button';
+import { useAuth } from '../contexts/AuthContext';
+
+export default function Home() {
+    const navigate = useNavigate();
+    const { logout } = useAuth();
+
+    function handleLogout() {
+        logout();
+        navigate('/auth');
+    }
+
+    return (
+        <div className="p-10">
+
+            <h1>Home Page</h1>
+
+            <p>
+                Authentication successful.
+            </p>
+
+            <div className="max-w-50">
+                <Button onClick={handleLogout}>
+                    Logout
+                </Button>
+            </div>
+
+        </div>
+    );
+}
