8895e6863a464c23e053d78198374ef8eed2eb3e
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-05-28 12:02:04 +0200

    removed public on main

--- src/main/java/com/app/javaapp/JavaAppApplication.java
+++ src/main/java/com/app/javaapp/JavaAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaAppApplication {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(JavaAppApplication.class, args);
     }
 }
