a783bed9ac55539de24089460c3d41a05e72f9e5
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-05-28 21:53:03 +0200

    created auth context to handle global auth state

---
+++ frontend/src/contexts/AuthContext.tsx
@@ -0,0 +1,83 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+interface User {
+    userId: number;
+    username: string;
+    email: string;
+}
+
+interface AuthContextType {
+    user: User | null;
+    token: string | null;
+    loading: boolean;
+    login: (token: string, user: User) => void;
+    logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export function AuthProvider({ children }: { children: React.ReactNode; }) {
+    const [user, setUser] = useState<User | null>(null);
+    const [token, setToken] = useState<string | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const storedToken = localStorage.getItem('token');
+        const storedUser = localStorage.getItem('user');
+
+        if (storedToken && storedUser) {
+
+            setToken(storedToken);
+
+            setUser(JSON.parse(storedUser));
+        }
+        setLoading(false);
+    }, []);
+
+    function login(token: string, user: User) {
+        localStorage.setItem('token', token);
+
+        localStorage.setItem(
+            'user',
+            JSON.stringify(user)
+        );
+
+        setToken(token);
+        setUser(user);
+    }
+
+    function logout() {
+
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+
+        setToken(null);
+        setUser(null);
+    }
+
+    return (
+        <AuthContext.Provider
+            value={{
+                user,
+                token,
+                loading,
+                login,
+                logout
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export function useAuth() {
+    const context = useContext(AuthContext);
+
+    if (!context) {
+        throw new Error(
+            'useAuth must be used inside AuthProvider'
+        );
+    }
+
+    return context;
+}
