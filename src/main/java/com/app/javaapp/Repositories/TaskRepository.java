839824a6179817c5e3da44b53fa8a0a819db3e30
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-05-29 10:51:28 +0200

    added JPA persistence

--- src/main/java/com/app/javaapp/Repositories/TaskRepository.java
+++ src/main/java/com/app/javaapp/Repositories/TaskRepository.java
@@ -1,4 +1,12 @@
 package com.app.javaapp.Repositories;
 
-public interface TaskRepository {
+import com.app.javaapp.Models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByCompleted(boolean completed);
 }
