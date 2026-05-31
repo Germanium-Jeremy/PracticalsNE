3d3982a401611fa7cd11d4882c3cfc0bd625d21c
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-05-31 22:04:29 +0200

    started Tasks services tests

--- src/test/java/com/app/javaapp/Services/TaskServicesTest.java
+++ src/test/java/com/app/javaapp/Services/TaskServicesTest.java
@@ -1,4 +1,60 @@
+package com.app.javaapp.Services;
+
+import com.app.javaapp.Models.Task;
+import com.app.javaapp.Repositories.TaskRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.stubbing.OngoingStubbing;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 class TaskServicesTest {
-  
+    private TaskRepository taskRepository;
+
+    @InjectMocks
+    private TaskServices taskServices;
+
+    public TaskServicesTest() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createTask() {
+    }
+
+    @Test
+    void getAllTasks() {
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task("New Task 1", "Task 1 Description"));
+        tasks.add(new Task("New Task 2", "Task 2 Description"));
+
+        OngoingStubbing<List<Task>> listOngoingStubbing = when(taskRepository.findAll()).thenReturn(Optional.of(tasks));
+    }
+
+    @Test
+    void getTaskById() {
+    }
+
+    @Test
+    void updateTask() {
+    }
+
+    @Test
+    void deleteTask() {
+    }
+
+    @Test
+    void getTasksByStatus() {
+    }
 }
