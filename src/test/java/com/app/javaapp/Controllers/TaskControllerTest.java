dad661592121237a58b0d95d838c817ee7edaf31
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-05-31 22:04:49 +0200

    started Tasks controller  tests

--- src/test/java/com/app/javaapp/Controllers/TaskControllerTest.java
+++ src/test/java/com/app/javaapp/Controllers/TaskControllerTest.java
@@ -1,4 +1,32 @@
+package com.app.javaapp.Controllers;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TaskControllerTest {
-  
+
+    @Test
+    void createTask() {
+    }
+
+    @Test
+    void getAllTasks() {
+    }
+
+    @Test
+    void getTaskById() {
+    }
+
+    @Test
+    void updateTask() {
+    }
+
+    @Test
+    void deleteTask() {
+    }
+
+    @Test
+    void getTasksByStatus() {
+    }
 }
